7f850f0cdd5aae09bcd123598f31f9c0d7d23874
ritvikir <ririgireddy1@gmail.com>
2023-07-17 00:45:32 -0700

    temp site

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex- col items-center justify-between p-24">
-      <h1>
-      </h1>
-    </main>
-  )
+    <div>
+      <main className="flex min-h-screen flex- col items-center p-20">
+        <h1>
+          welcome to ritvik.xyz - currently still building this site. in the
+          meanwhile checkout my github/twitter/insta @ritvikir
+        </h1>
+      </main>
+    </div>
+  );
 }

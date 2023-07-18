2dcd600bc85906ef32635ce429556f9bec65df4e
ritvikir <ririgireddy1@gmail.com>
2023-07-17 20:36:54 -0700

    finish nav

--- app/page.js
+++ app/page.js
@@ -1,14 +1,31 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <main className="flex min-h-screen flex- col items-center p-20">
-        <h1>
-          welcome to ritvik.xyz - currently still building this site. in the
-          meanwhile checkout my github/twitter/insta @ritvikir
-        </h1>
-      </main>
+    <div className="max-w-2xl mx-auto">
+      <nav className="font-semibold px-4 py-6 ">
+        <div className="container flex flex-wrap justify-between items-center mx-auto">
+          <span className="flex items-center self-center ">ritvik.xyz</span>
+          <div className="  w-auto" id="navbar-default">
+            <ul className="flex flex-row space-x-8 mt-0 ">
+              <li className=" py-1 ">
+                <Link href="" aria-current="page">
+                  github
+                </Link>
+              </li>
+              <li className="py-1 ">
+                <Link href="" aria-current="page">
+                  twitter
+                </Link>
+              </li>
+              <li className=" py-1 ">
+                <Link href="">words</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
